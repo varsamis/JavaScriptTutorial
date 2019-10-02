c368994b3629cd9f91dd6b1a9e33141027a4a1be
Varsamis Karavarsamis <varsamisk@gmail.com>
2019-10-02 13:55:06 +0200

    Add .giignore file

--- test.js
+++ test.js
@@ -9,3 +9,17 @@ var myBody = document.getElementsByTagName("body")[0];
 myNode.appendChild(myTextNode);
 
 myBody.appendChild(myNode);
+
+document.addEventListener("DOMContentLoaded", onPageLoad);
+
+function onPageLoad(){
+  let theButton = document.getElementById("myButton");
+  theButton.addEventListener("click", handleButtonClick);
+  console.log("Added Eventlistener");
+}
+
+function handleButtonClick(){
+  console.log("Button clicked");
+  let theButton = document.getElementById("myButton");
+  theButton.removeEventListener("click", handleButtonClick);
+}
